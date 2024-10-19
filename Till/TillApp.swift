8e05be8578ae4c0657f20effa72752d2cb5a61cb
Tyler Sudol <sudol.tyler@gmail.com>
2024-10-19 18:19:33 -0400

    Initial commit

--- Till/TillApp.swift
+++ Till/TillApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TillApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TillView()
         }
     }
 }
---
+++ Till/Views/ThemeView.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeView.swift
+//  Till
+//
+//  Created by Tyler Sudol on 10/19/24.
+//
+
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
