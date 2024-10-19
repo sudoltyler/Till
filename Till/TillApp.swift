c1480898c6a1905ec6d5a7deaabb161a03053169
Tyler Sudol <sudol.tyler@gmail.com>
2024-10-19 18:13:16 -0400

    Initial Commit

---
+++ Till/TillApp.swift
@@ -0,0 +1,17 @@
+//
+//  TillApp.swift
+//  Till
+//
+//  Created by Tyler Sudol on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TillTests/TillTests.swift
@@ -0,0 +1,17 @@
+//
+//  TillTests.swift
+//  TillTests
+//
+//  Created by Tyler Sudol on 10/19/24.
+//
+
+import Testing
+@testable import Till
+
+struct TillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
